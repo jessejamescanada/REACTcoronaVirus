62f2381fd0e26e767ad6f5eb1fb2f3ec7a3d7867
Jesse Giammattolo <giammattolo@hotmail.com>
2020-03-16 14:51:36 -0400

    cleaned up white space in code

--- src/App.js
+++ src/App.js
@@ -21,10 +21,3 @@ function App() {
 
 export default App;
 
-// {item.title.length > 100
-//   ? `${item.title.substring(0, 100)}...`
-//   : item.title}
-
-//   {item.description.length > 200
-//     ? `${item.description.substring(0, 200)}...`
-//     : item.description}
